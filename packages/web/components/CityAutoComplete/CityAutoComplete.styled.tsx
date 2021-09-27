39403a829384bb03e800ba305d08b90ba8815fd4
fabien-h <fabien.huet@gmail.com>
2021-09-27 22:52:48 +0200

    feat(autocomplete): autocomplete component

---
+++ packages/web/components/CityAutoComplete/CityAutoComplete.styled.tsx
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+import * as COLORS from '@locadocs/design-system/constants/colors';
+import * as SPACINGS from '@locadocs/design-system/constants/spacings';
+
+export const Container = styled.div`
+  > .results {
+    background: ${COLORS.G200};
+    overflow: auto;
+    max-height: 30rem;
+
+    > a {
+      display: flex;
+      align-items: center;
+      text-decoration: none;
+      justify-content: space-between;
+      padding: 0 ${SPACINGS.SPACING_4W};
+      height: ${SPACINGS.SPACING_FINGER};
+      border-bottom: 1px solid ${COLORS.G400};
+      transition: 200ms ease-out color, 200ms ease-out background-color;
+
+      &:last-of-type {
+        border-bottom: none;
+      }
+
+      > svg {
+        transition: 200ms ease-out fill;
+        display: block;
+        width: ${SPACINGS.SPACING_4W};
+      }
+
+      &:hover {
+        color: ${COLORS.WHITE};
+        background-color: ${COLORS.BLUE_FRANCE};
+
+        > svg {
+          fill: ${COLORS.WHITE};
+        }
+      }
+    }
+  }
+`;
--- packages/web/components/CityAutoComplete/CityAutoComplete.tsx
+++ packages/web/components/CityAutoComplete/CityAutoComplete.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
-import { Container } from './CityAutoCompleteStyles';
+import { Container } from './CityAutoComplete.styled';
 import { Input } from '@locadocs/design-system/components/Input';
+import { Loader } from '@locadocs/design-system/components/Loader';
+import NextLink from 'next/link';
+import Icon from '@mdi/react';
+import { mdiArrowRight } from '@mdi/js';
 
 type TCity = {
   inseeCode: string;
@@ -9,31 +13,73 @@ type TCity = {
   subCities: Array<string>;
 };
 
-export const CityAutoComplete: React.FC<unknown> = (): JSX.Element => {
-  const [citiesFound, setCitiesFound] = React.useState<Array<TCity>>([]);
+type TProps = {
+  buildLink: (cityCode: string) => string;
+};
 
+export const CityAutoComplete: React.FC<TProps> = ({
+  buildLink,
+}): JSX.Element => {
+  const [citiesFound, setCitiesFound] = React.useState<Array<TCity>>([]);
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  const [cityNotFound, setCityNotFound] = React.useState<boolean | string>(
+    false,
+  );
+  let timeoutId: NodeJS.Timer;
   const onChange = async (event: React.FormEvent<HTMLInputElement>) => {
     const userInput = event.currentTarget.value;
+    clearTimeout(timeoutId);
+    setCityNotFound(false);
+
     if (userInput.length < 3) {
       setCitiesFound([]);
       return;
     }
 
-    const result = await fetch(`/api/get-cities?search=${userInput}`);
-    const { result: cities } = await result.json();
+    timeoutId = setTimeout(async () => {
+      setIsLoading(true);
 
-    setCitiesFound(cities || []);
+      const result = await fetch(`/api/get-cities?search=${userInput}`);
+      const { result: cities } = await result.json();
+
+      setCitiesFound(cities || []);
+      setIsLoading(false);
+      if (!cities || cities.length === 0) {
+        setCityNotFound(userInput);
+      }
+    }, 200);
   };
 
   return (
     <Container>
-      <Input label="Entrez une ville" inputProps={{ onChange }} />
-      {citiesFound.length > 0 &&
-        citiesFound.map(city => (
-          <div key={city.inseeCode}>
-            {city.name} - {city.postalCode} -
-          </div>
-        ))}
+      <Input
+        label="Entrez une ville"
+        inputProps={{ onChange }}
+        error={cityNotFound ? `Pas de ville pour ${cityNotFound}` : undefined}
+      />
+
+      {isLoading && <Loader />}
+
+      {citiesFound?.length > 0 && (
+        <div className="results">
+          {citiesFound.map(city => {
+            let label = `${city.name}`;
+            if (city.subCities && city.subCities.length > 0) {
+              label += ` (${city.subCities.join(', ')})`;
+            }
+            label += ` - ${city.postalCode}`;
+
+            return (
+              <NextLink key={city.inseeCode} href={buildLink(city.inseeCode)}>
+                <a href={buildLink(city.inseeCode)}>
+                  <span>{label}</span>
+                  <Icon path={mdiArrowRight} />
+                </a>
+              </NextLink>
+            );
+          })}
+        </div>
+      )}
     </Container>
   );
 };
