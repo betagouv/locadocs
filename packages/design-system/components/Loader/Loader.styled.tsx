b8a90130cc75360144d809d3945f2a0a11500cae
fabien-h <fabien.huet@gmail.com>
2021-09-27 22:36:37 +0200

    feat(loader): add loader component

---
+++ packages/design-system/components/Loader/Loader.styled.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const SContainer = styled.span`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  pointer-events: none;
+  width: 100%;
+  height: 100%;
+
+  > svg {
+    display: block;
+  }
+`;
