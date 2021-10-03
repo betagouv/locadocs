378c2f956c6062e3004e6947586043b8ebd3626e
fabien-h <fabien.huet@gmail.com>
2021-10-03 23:46:36 +0200

    feat(front): front call for the mortage registry retriever api

--- packages/web/pages/search-estate/result-mortgage-registry/[inseeCode].tsx
+++ packages/web/pages/search-estate/result-mortgage-registry/[inseeCode].tsx
@@ -5,9 +5,28 @@ import { Button, EButtonKind } from '@locadocs/design-system/components/Button';
 import * as TITLES from '@locadocs/design-system/components/Title';
 import { mdiArrowLeft } from '@mdi/js';
 import { GLOBALS } from '../../../static-data/static-search-estate';
+import { useEffect } from 'react';
 
 const ResultBuildingPermit = (): JSX.Element => {
+  let isMounted = true;
   const router = useRouter();
+  const loadData = async (): Promise<void> => {
+    const result = await fetch(
+      `/api/get-mortgage-registry?inseeCode=${router.query.inseeCode}`,
+    );
+    const jsonResult = await result.json();
+    if (isMounted) {
+      // eslint-disable-next-line no-console
+      console.log(jsonResult);
+    }
+  };
+
+  useEffect(() => {
+    loadData();
+    return (): void => {
+      isMounted = false;
+    };
+  }, []);
 
   return (
     <SiteLayout>
