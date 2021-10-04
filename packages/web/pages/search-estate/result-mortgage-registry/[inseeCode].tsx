a9b777f0ec52a0d93ee8940657c437b8cefa19a4
fabien-h <fabien.huet@gmail.com>
2021-10-04 22:24:11 +0200

    feat(front): add data in the mortage registry results

--- packages/web/pages/search-estate/result-mortgage-registry/[inseeCode].tsx
+++ packages/web/pages/search-estate/result-mortgage-registry/[inseeCode].tsx
@@ -5,41 +5,81 @@ import { Button, EButtonKind } from '@locadocs/design-system/components/Button';
 import * as TITLES from '@locadocs/design-system/components/Title';
 import { mdiArrowLeft } from '@mdi/js';
 import { GLOBALS } from '../../../static-data/static-search-estate';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { Loader } from '@locadocs/design-system/components/Loader';
+import type { City } from '@locadocs/shared/types/City';
+import type { MortgageRegistry } from '@locadocs/shared/types/MortgageRegistry';
 
 const ResultBuildingPermit = (): JSX.Element => {
   let isMounted = true;
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [city, setCity] = useState<undefined | City>();
+  const [mortgageRegistry, setMortgageRegistry] = useState<
+    undefined | MortgageRegistry
+  >();
   const loadData = async (): Promise<void> => {
+    setIsLoading(true);
+    const { inseeCode } = router.query;
+
+    if (!inseeCode) {
+      return;
+    }
+
     const result = await fetch(
-      `/api/get-mortgage-registry?inseeCode=${router.query.inseeCode}`,
+      `/api/get-mortgage-registry?inseeCode=${inseeCode}`,
     );
-    const jsonResult = await result.json();
+    const { city, mortgageRegistry } = await result.json();
+
     if (isMounted) {
-      // eslint-disable-next-line no-console
-      console.log(jsonResult);
+      setCity(city);
+      setMortgageRegistry(mortgageRegistry);
+      setIsLoading(false);
     }
   };
 
   useEffect(() => {
     loadData();
+
     return (): void => {
       isMounted = false;
     };
-  }, []);
+  }, [router.query]);
 
   return (
     <SiteLayout>
       <StaticData baseLine={GLOBALS.baseLine}>
-        <TITLES.H1>Registre des hypothèques pour...</TITLES.H1>
+        {isLoading && <Loader />}
 
-        {router.query.inseeCode}
+        {!isLoading && city && mortgageRegistry && (
+          <>
+            <TITLES.H1>
+              Registre des hypothèques pour {city.name} - {city.postalCode}
+              {city?.subCities.length > 0 && ` (${city.subCities.join(', ')})`}
+            </TITLES.H1>
 
-        <Button
-          onClick={() => router.back()}
-          leftIcon={mdiArrowLeft}
-          kind={EButtonKind.TERTIARY}
-        />
+            <p>
+              <strong>{mortgageRegistry.title}</strong> -{' '}
+              {mortgageRegistry.districtNumber} -{' '}
+              {mortgageRegistry.districtName}
+            </p>
+
+            <ul>
+              {mortgageRegistry.address && <li>{mortgageRegistry.address}</li>}
+              {mortgageRegistry.email && <li>{mortgageRegistry.email}</li>}
+              {mortgageRegistry.phone && <li>{mortgageRegistry.phone}</li>}
+              {mortgageRegistry.moreInfos && (
+                <li>{mortgageRegistry.moreInfos}</li>
+              )}
+            </ul>
+
+            <Button
+              onClick={() => router.back()}
+              leftIcon={mdiArrowLeft}
+              kind={EButtonKind.TERTIARY}
+            />
+          </>
+        )}
       </StaticData>
     </SiteLayout>
   );
