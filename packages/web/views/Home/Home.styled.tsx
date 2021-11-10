dc3b88a3e21e90416a88462454234067ce489745
fabien-h <fabien.huet@gmail.com>
2021-11-10 09:48:03 +0100

    home styles

--- packages/web/views/Home/Home.styled.tsx
+++ packages/web/views/Home/Home.styled.tsx
@@ -4,15 +4,14 @@ import * as COLORS from '@locadocs/design-system/constants/colors';
 import * as SPACINGS from '@locadocs/design-system/constants/spacings';
 
 export const Container = styled.div`
-  padding-bottom: ${SPACINGS.SPACING_10W};
-
   > header {
     width: 100%;
+    border-bottom: 1px solid #eee;
 
     > div {
       width: 100%;
       max-width: 110rem;
-      margin: 0 auto;
+      margin: 3rem auto;
       padding: ${SPACINGS.SPACING_4W};
       display: flex;
       align-items: center;
@@ -25,7 +24,9 @@ export const Container = styled.div`
 
       > div {
         > h1 {
+          font-size: 3.6rem;
           margin-bottom: ${SPACINGS.SPACING_12W};
+          max-width: 57rem;
         }
 
         > ul {
@@ -35,37 +36,132 @@ export const Container = styled.div`
 
       > img {
         display: block;
-        max-width: 25rem;
+        max-width: 50rem;
         width: 100%;
         height: auto;
         margin: ${SPACINGS.SPACING_6W} auto;
 
         @media (min-width: ${BREAKPOINTS.MD}) {
-          width: 30rem;
           margin-left: ${SPACINGS.SPACING_8W};
         }
       }
     }
   }
 
-  > section {
-    background-color: ${COLORS.BEIGE};
+  > .info {
+    display: flex;
     width: 100%;
-    max-width: 110rem;
-    margin: ${SPACINGS.SPACING_4W} auto;
-    padding: ${SPACINGS.SPACING_4W} ${SPACINGS.SPACING_6W};
+    max-width: 80rem;
+    margin: 5rem auto;
+    padding: ${SPACINGS.SPACING_4W};
+    flex-direction: column;
 
-    > h2 {
-      margin-bottom: ${SPACINGS.SPACING_4W};
+    > div:first-of-type {
+      flex-shrink: 0;
+      padding-right: 5rem;
+      width: 30rem;
+
+      > span {
+        display: block;
+        font-size: 3rem;
+        line-height: 3.4rem;
+        font-weight: 600;
+
+        &:last-of-type {
+          color: ${COLORS.BLUE_FRANCE_300};
+        }
+      }
+
+      &::after {
+        display: block;
+        content: '';
+        height: 2px;
+        width: 10rem;
+        background-color: ${COLORS.BLUE_FRANCE_300};
+        margin-top: 3rem;
+      }
+    }
+
+    > div:last-of-type {
+      line-height: 2.5rem;
+
+      > ul {
+        padding: 1rem 0 1rem 2rem;
+
+        > li {
+          margin: 0.5rem 0;
+        }
+      }
+    }
+
+    @media (min-width: ${BREAKPOINTS.MD}) {
+      flex-direction: row;
+    }
+  }
+
+  > .bulletPoints {
+    background-color: ${COLORS.BLUE_FRANCE_200};
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    margin: 10rem 0;
+
+    > div {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      margin: 5rem;
+
+      > span {
+        display: block;
+        font-size: 4rem;
+        text-align: center;
+      }
+
+      > p {
+        max-width: 20rem;
+        text-align: center;
+      }
+    }
+  }
+
+  > .genericQuestions {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    max-width: 94rem;
+    margin: 10rem auto;
+    padding: 0 2rem;
+    flex-wrap: wrap;
+
+    > img {
+      display: block;
+      max-width: 45rem;
+      width: 100%;
+      height: auto;
     }
 
     > p {
-      margin: ${SPACINGS.SPACING_4W} 0;
+      font-weight: 600;
+      max-width: 45rem;
+      width: 100%;
+      font-size: 2.8rem;
     }
+  }
 
-    > ul {
-      margin: ${SPACINGS.SPACING_4W} 0;
-      margin-left: ${SPACINGS.SPACING_6W};
-    }
+  > .cta {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 15rem 0;
+  }
+
+  > .contact {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 10rem;
+    background-color: ${COLORS.BLUE_FRANCE_600};
+    color: ${COLORS.WHITE};
   }
 `;
--- packages/web/views/Home/Home.tsx
+++ packages/web/views/Home/Home.tsx
@@ -12,52 +12,146 @@ export const Home: React.FC<unknown> = (): JSX.Element => {
         <div>
           <div>
             <TITLE.H1>
-              Simplifions l’accès des usagers aux documents administratifs
-              publics
+              Vous cherchez un document ou une information sur un terrain, une
+              maison ou un immeuble&nbsp;?
             </TITLE.H1>
 
-            <ul>
-              <li>À qui dois-je m’adresser&nbsp;?</li>
-              <li>Est-ce que je peux consulter le document&nbsp;?</li>
-              <li>Vais-je y trouver l’information que je cherche&nbsp;?</li>
-            </ul>
+            <Link
+              label="Je commence ma recherche"
+              href={ROUTES.ESTATE}
+              leftIcon={mdiArrowRight}
+            />
           </div>
 
           <img
-            src="/imgs/home_illustration.svg"
+            src="/imgs/home_1.png"
             alt="woman searching for a file"
-            width="578"
-            height="622"
+            width="640"
+            height="360"
           />
         </div>
       </header>
 
-      <section>
-        <TITLE.H2>Trouver un document sur un bien immobilier</TITLE.H2>
+      <section className="info">
+        <div>
+          <span>Comment</span>
+          <span>ça marche&nbsp;?</span>
+        </div>
 
-        <p>
-          Vous cherchez <strong>un document ou une information</strong> sur{' '}
-          <strong>un terrain, une maison ou un immeuble</strong>&nbsp;?
-          <br />
-          Nous avons créé ce guide pour vous aider à&nbsp;:
-        </p>
+        <div>
+          <strong>Trouver un document</strong> détenu par l'administration
+          relève parfois du parcours du combattant&nbsp;!{' '}
+          <strong>Loc@docs</strong> est un guide en ligne qui facilite vos
+          recherches de{' '}
+          <strong>documents concernant les biens immobiliers</strong>. Il vous
+          aide en quelques clics à savoir:
+        </div>
+      </section>
 
-        <ul>
-          <li>
-            <strong>Identifier le document administratif</strong> susceptible de
-            contenir l'information que vous recherchez.
-          </li>
-          <li>
-            <strong>Vous orienter vers le bon contact</strong> pour consulter ou
-            obtenir une copie de ce document.
-          </li>
-        </ul>
-
-        <Link
-          label="Commencer"
-          href={ROUTES.ESTATE}
-          rightIcon={mdiArrowRight}
+      <section className="genericQuestions">
+        <img
+          src="/imgs/home_2.png"
+          alt="man looking at a big question mark"
+          width="640"
+          height="360"
         />
+        <p>
+          1. Quel document administratif contient l'information que je
+          cherche&nbsp;?
+        </p>
+      </section>
+
+      <section className="genericQuestions">
+        <p>2. Où se trouve ce document et comment faire ma demande&nbsp;?</p>
+        <img
+          src="/imgs/home_3.png"
+          alt="man typing on a laptop"
+          width="600"
+          height="700"
+        />
+      </section>
+
+      <section className="bulletPoints">
+        <div>
+          <span>📑</span>
+          <p>
+            Vous cherchez un permis de construire&nbsp;? Un acte de vente&nbsp;?
+          </p>
+        </div>
+
+        <div>
+          <span>📍</span>
+          <p>Vous voulez connaître les limites d'une propriété&nbsp;?</p>
+        </div>
+
+        <div>
+          <span>🏠</span>
+          <p>
+            Vous cherchez une information sur la mitoyenneté d'un mur&nbsp;?
+          </p>
+        </div>
+      </section>
+
+      <section className="cta">
+        <Link
+          label="Je commence ma recherche"
+          href={ROUTES.ESTATE}
+          leftIcon={mdiArrowRight}
+        />
+      </section>
+
+      <section className="info">
+        <div>
+          <span>Que trouverez-vous dans</span>
+          <span>Loc@docs</span>
+        </div>
+
+        <div>
+          <strong>Le contenu et le lieu de conservation :</strong>
+          <ul>
+            <li>d’un permis de construire</li>
+            <li>du cadastre</li>
+            <li>d’un registre des hypothèques</li>
+            <li>
+              d’un acte notarié (actes de vente, contrats de donation,
+              testaments, etc)
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <section className="info">
+        <div>
+          <span>A propos de</span>
+          <span>Loc@docs</span>
+        </div>
+
+        <div>
+          Loc@docs est un projet porté par le{' '}
+          <a
+            href="https://francearchives.fr/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Service interministériel des Archives de France
+          </a>{' '}
+          (ministère de la culture) actuellement en phase d'expérimentation. Il
+          est accompagné par l'Atelier numérique, l'incubateur du ministère de
+          la culture, et s'inscrit dans le cadre du programme interministériel
+          <a
+            href="https://beta.gouv.fr/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            beta.gouv.fr
+          </a>
+          , qui soutient les administrations publiques dans la construction de
+          services numériques utiles, simples, faciles à utiliser.
+        </div>
+      </section>
+
+      <section className="contact">
+        <a href="mailto:locadocs@beta.gouv.fr">Nous contacter</a>
       </section>
     </Container>
   );
